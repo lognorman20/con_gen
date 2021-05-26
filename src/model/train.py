3799a6748567bc1a9ae6432c36c1cded1059d1fe
lognorman20 <logannormanss@gmail.com>
2021-05-25 22:20:53 -0600

    init

--- src/model/train.py
+++ src/model/train.py
@@ -4,4 +4,39 @@ sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 
 from data import make_corpus as tp
 
+from numpy import array
+from pickle import dump
+from keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
+
 corpus = tp.load_corpus('/Users/logno/Documents/GitHub/conspiracy_generation/data/processed/seqs.txt')
+lines = corpus.split('\n')
+
+# integer encode sequences of words
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(lines)
+sequences = tokenizer.texts_to_sequences(lines)
+
+vocab_size = len(tokenizer.word_index) + 1
+
+sequences = array(sequences)
+
+X, y = sequences[:,:-1], sequences[:,-1]
+y = to_categorical(y, num_classes=vocab_size)
+seq_length = X.shape[1]
+
+model = Sequential()
+model.add(Embedding(vocab_size, 50, input_length=seq_length))
+model.add(LSTM(100, return_sequences=True))
+model.add(LSTM(100))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(vocab_size, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(X, y, batch_size=128, epochs=100)
