bf835e5404380f8739980ce6fe48ec8e8377ea2f
lognorman20 <logannormanss@gmail.com>
2021-05-25 23:25:59 -0600

    init

---
+++ src/model/generate.py
@@ -0,0 +1,32 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import to_categorical
+from numpy.random import randint
+from pickle import load
+
+model = load_model('/Users/logno/Documents/GitHub/conspiracy_generation/models/model.h5')
+tokenizer = load(open('/Users/logno/Documents/GitHub/conspiracy_generation/models/tokenizer.pkl', 'rb'))
+
+# generate a sequence from a language model
+def generate_seq(model, tokenizer, seq_length, seed_text, n_words):
+	result = list()
+	in_text = seed_text
+	# generate a fixed number of words
+	for _ in range(n_words):
+		# encode the text as integer
+		encoded = tokenizer.texts_to_sequences([in_text])[0]
+		# truncate sequences to a fixed length
+		encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
+		# predict probabilities for each word
+		yhat = model.predict_classes(encoded, verbose=0)
+		# map predicted word index to word
+		out_word = ''
+		for word, index in tokenizer.word_index.items():
+			if index == yhat:
+				out_word = word
+				break
+		# append to input
+		in_text += ' ' + out_word
+		result.append(out_word)
+	return ' '.join(result)
