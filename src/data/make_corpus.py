31403ecdf39ed3a9a462394b0ac610ad843eb544
lognorman20 <logannormanss@gmail.com>
2021-05-25 20:56:19 -0600

    split parse func, create get_tokens

--- src/data/make_corpus.py
+++ src/data/make_corpus.py
@@ -15,20 +15,26 @@ def load_corpus(filename):
     return text
 
 # Parse & Clean Function Testing
+# Parse & Clean Function Testing
 def parse_and_clean(input):
     with open('../data/processed/corpus.txt', 'w') as fout:
         text = load_corpus(input)
         text = re.sub(r'http\S+', '', text) # removes hyperlinks
         text = text.replace('_', "") # removes underscores
         text = text.replace('  (', "") # removes empty parentheses
-        text = text.replace('* ', "") # removes empty parentheses
+        text = text.replace('* ', "") # removes * characters
+        text = text.replace('"', "") # removes * characters
 
         fout.write(text)
+        return text
 
-        tokens = text.split() # create tokens
-        table = str.maketrans('', '', string.punctuation)
-        tokens = [w.translate(table) for w in tokens]
-        tokens = [word for word in tokens if word.isalpha()] # remove remaining tokens that are not alphabetic
-        tokens = [word.lower() for word in tokens] # make lower case
+def get_tokens(input):
+    text = parse_and_clean(input) # read & clean data
+    
+    tokens = text.split() # create tokens
+    table = str.maketrans('', '', string.punctuation)
+    tokens = [w.translate(table) for w in tokens]
+    tokens = [word for word in tokens if word.isalpha()] # remove remaining tokens that are not alphabetic
+    tokens = [word.lower() for word in tokens] # make lower case
 
-        return tokens
+    return tokens
