4db986e4216a14f383e69af331d3c840d66ceed8
lognorman20 <logannormanss@gmail.com>
2021-05-30 21:28:17 -0600

    added all theory urls

--- src/data/scrape.py
+++ src/data/scrape.py
@@ -15,10 +15,13 @@ reddit = praw.Reddit(
     )
 
 for url in config.post_urls:
-    submission = reddit.submission(url=url)
+    try:
+        submission = reddit.submission(url=url)
 
-    submission.comments.replace_more(limit=None)
+        submission.comments.replace_more(limit=None)
 
-    with open('/Users/logno/Documents/GitHub/conspiracy_generation/data/raw/conspiracy_theories.txt', 'w') as fout:
-        for comment in submission.comments.list():
-            fout.write(comment.body.replace("\n", "") + '\n' * 2)
+        with open('/Users/logno/Documents/GitHub/conspiracy_generation/data/raw/conspiracy_theories.txt', 'w') as fout:
+            for comment in submission.comments.list():
+                fout.write(comment.body.replace("\n", "") + '\n' * 2)
+    except:
+        pass
