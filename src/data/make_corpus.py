640583b73f33116e5c2b682f4deb26b6bf75ec19
lognorman20 <logannormanss@gmail.com>
2021-05-31 18:41:18 -0600

    removed other funs, only parse_clean

--- src/data/make_corpus.py
+++ src/data/make_corpus.py
@@ -1,35 +1,30 @@
 import re
 import string
 
-### Functions in this file ###
-# 1. load_corpus - reads in raw data
-# 2. parse_and_clean - cleans raw corpus for tokenization
+filename = "/Users/logno/Documents/GitHub/conspiracy_generation/data/raw/conspiracy_theories.txt"
 
-# Cleaning tasks:
-#     -> Remove short comments (less than 170 characters)
-#     -> Remove image links (, .jpg, .png)
-#     -> Remove comments that are just a link
-#     -> Remove comments with more than three links
-#     -> Remove comments using quote notation (">" or ">>" markdown symbols)
-#     -> Remove trailing, starting, and double white space from each comment
-#     -> Remove non-ASCII characters
+def parse_clean(filename):
+    '''
+    Cleaning tasks:
+    -> Remove short comments (less than 170 characters)
+    -> Remove comments with links
+    -> Remove comments using quote notation (">" or ">>" markdown symbols)
+    -> Remove double white space from each comment
+    -> Remove non-ASCII characters
+    '''
+    with open(filename, 'r') as fin:
+        with open('/Users/logno/Documents/GitHub/conspiracy_generation/data/interim/corpus.txt', 'w') as fout:
+            for line in fin:
+                if len(line) >= 100:
+                    line = line.replace('>', '') 
+                    line = line.replace('  ', ' ') 
+                    line = re.sub(r'http\S+', '', line)
+                    line = line.replace('>', '')
+                    encoded_string = line.encode("ascii", "ignore")
+                    line = encoded_string.decode()
+                    
+                    fout.write(line)
+        
+    print("The corpus has been created at '/Users/logno/Documents/GitHub/conspiracy_generation/data/interim/corpus.txt'")
 
-# Load raw corpus
-def load_corpus(filename):
-    '''
-    This functions reads in the raw data file to be processed.
-    '''
-    file = open(filename, 'r') # open the file as read only
-    text = file.read() # read all text
-    file.close() # close the file
-    return text
-
-def remove_shorties(filename):
-    '''
-    Removes short comments (less than 170 characters)
-    '''
-    with open(filename, 'r') as fin, open('../data/interim/corpus.txt', 'w') as fout:
-        for line in fin:
-            if len(line) > 170:
-                fout.write(line)
-
+parse_clean(filename)
