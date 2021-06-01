bcd817df63e33d91683e0608e5c835d32f3f14b4
lognorman20 <logannormanss@gmail.com>
2021-05-31 22:33:23 -0600

    update

--- src/model/generate.py
+++ src/model/generate.py
@@ -1,42 +1,16 @@
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import load_model
-from tensorflow.keras.utils import to_categorical
-from numpy.random import randint
-from pickle import load
+import gpt_2_simple as gpt2
 
-model = load_model('/Users/logno/Documents/GitHub/conspiracy_generation/models/lstm_1.h5')
-tokenizer = load(open('/Users/logno/Documents/GitHub/conspiracy_generation/models/tokenizer_1.pkl', 'rb'))
-print('Model & Tokenizer Loaded')
+sess = gpt2.start_tf_sess()
+gpt2.load_gpt2(sess, run_name="run1", checkpoint_dir='/Users/logno/Documents/GitHub/conspiracy_generation/models/checkpoint/',
+model_dir='/Users/logno/Documents/GitHub/conspiracy_generation/models/', multi_gpu=False)
 
-# generate a sequence from a language model
-def generate_seq(model, tokenizer, seq_length, seed_text, n_words):
-	result = list()
-	in_text = seed_text
-	# generate a fixed number of words
-	for _ in range(n_words):
-		# encode the text as integer
-		encoded = tokenizer.texts_to_sequences([in_text])[0]
-		# truncate sequences to a fixed length
-		encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
-		# predict probabilities for each word
-		yhat = model.predict_classes(encoded, verbose=0)
-		# map predicted word index to word
-		out_word = ''
-		for word, index in tokenizer.word_index.items():
-			if index == yhat:
-				out_word = word
-				break
-		# append to input
-		in_text += ' ' + out_word
-		result.append(out_word)
-	return ' '.join(result)
+text = gpt2.generate(sess,
+              length=50,
+              temperature=0.9,
+              top_p=0.9,
+              prefix="The moon landing is fake because",
+              nsamples=3,
+              truncate='<|endoftext|>'
+              )
 
-# select a seed text
-seed_text = 'Donald Trump is'
-seq_length = 50
-print(seed_text + '\n')
- 
-# generate new text
-generated = generate_seq(model, tokenizer, seq_length, seed_text, 50)
-print(generated)
+print(text)
--- src/model/train.py
+++ src/model/train.py
@@ -1,50 +1,19 @@
-import os, sys
-from os.path import dirname, join, abspath
-sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+import gpt_2_simple as gpt2
 
-from data import make_corpus as tp
+# downloading model
+gpt2.download_gpt2(model_name="124M")
 
-from numpy import array
-from pickle import dump
-from keras.preprocessing.text import Tokenizer
-from tensorflow.keras.utils import to_categorical
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Embedding
+file_name = "/Users/logno/Documents/GitHub/conspiracy_generation/data/interim/corpus.txt"
 
-# loading in train corpus
-corpus = tp.load_corpus('/Users/logno/Documents/GitHub/conspiracy_generation/data/processed/seqs.txt')
-lines = corpus.split('\n')
+# train model on corpus
+sess = gpt2.start_tf_sess()
 
-# integer encode sequences of words
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(lines)
-sequences = tokenizer.texts_to_sequences(lines)
-
-vocab_size = len(tokenizer.word_index) + 1
-
-sequences = array(sequences)
-
-X, y = sequences[:,:-1], sequences[:,-1]
-y = to_categorical(y, num_classes=vocab_size)
-seq_length = X.shape[1]
-
-# model definition
-model = Sequential()
-model.add(Embedding(vocab_size, 50, input_length=seq_length))
-model.add(LSTM(100, return_sequences=True))
-model.add(LSTM(100))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(vocab_size, activation='softmax'))
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# training the model
-model.fit(X, y, batch_size=128, epochs=100)
-
-# save the model to file
-model.save('lstm.h5')
-
-# save the tokenizer
-dump(tokenizer, open('tokenizer_1.pkl', 'wb'))
+gpt2.finetune(sess,
+              dataset=file_name,
+              model_name='124M',
+              steps=200,
+              restore_from='fresh',
+              run_name='run1',
+              print_every=10,
+              sample_every=100,
+              )
