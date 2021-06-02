bbee462c06b0ab675c0e556a16d5ebb65a30d32c
lognorman20 <logannormanss@gmail.com>
2021-06-02 16:42:39 -0600

    changed prompt

--- src/model/generate.py
+++ src/model/generate.py
@@ -7,7 +7,7 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 ai = aitextgen(model_folder="/Users/logno/Documents/GitHub/conspiracy_generation/models/trained_model", to_gpu=False)
 
 text = ai.generate(n=5,
-            prompt="The moon landing",
+            prompt="The moon landing is",
             min_length=100,
             temperature=1.0,
             top_p=0.9)
