c0595d9f2e8a795087e0fbae2c9a58ff517e13ba
lognorman20 <logannormanss@gmail.com>
2021-06-02 18:55:07 -0600

    update func

--- src/model/generate.py
+++ src/model/generate.py
@@ -6,10 +6,7 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 ai = aitextgen(model_folder="/Users/logno/Documents/GitHub/conspiracy_generation/models/trained_model", to_gpu=False)
 
-text = ai.generate(n=5,
-            prompt="The moon landing is",
-            min_length=100,
-            temperature=1.0,
-            top_p=0.9)
+def generate(user_input):
+    return ai.generate(n=5, prompt=user_input, min_length=100, temperature=1.0, top_p=0.9)
 
-print(text)
+generate("The moon landing is")
